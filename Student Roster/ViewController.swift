c73df0940f8831328eda8b0c1cbd710fec22ef4c
Jennifer Joseph <josephjz@bc.edu>
2020-11-21 08:54:45 -0500

    Initial Commit

---
+++ Student Roster/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Student Roster
+//
+//  Created by Jennifer Joseph on 11/21/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
